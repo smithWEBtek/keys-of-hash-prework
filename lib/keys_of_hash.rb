8b656488b22db31f932661ef3fc38d63a83c4bb2
smithwebtek <brad@smithwebtek.com>
2018-04-06 23:56:31 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+	def keys_of(*args)
+		map {|key, value| args.include?(value) ? key : nil }.compact
+	end
 end
+
+# meals = Hash.new
+# meals["breakfast"] = 'eggs'
+# meals["lunch"] = 'sandwich'
+# meals["dinner"] = 'soup'
+
+# data = ['turtle', 'soup', 'juice']
+# meals.keys_of(data)
